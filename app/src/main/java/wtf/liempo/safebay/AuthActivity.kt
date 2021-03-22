27dcc2fa071c6e2f3f4c6e2b478310bb48159172
Alec John <gonzalesalec@gmail.com>
2021-03-22 23:53:52 +0800

    Add Firebase UI authentication

--- app/src/main/java/wtf/liempo/safebay/AuthActivity.kt
+++ app/src/main/java/wtf/liempo/safebay/AuthActivity.kt
@@ -2,10 +2,33 @@ package wtf.liempo.safebay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
 
 class AuthActivity : AppCompatActivity() {
+
+    private val provider by lazy {
+        AuthUI.IdpConfig.PhoneBuilder()
+                .setWhitelistedCountries(listOf("PH"))
+                .build()
+    }
+
+    private val auth by lazy {
+        AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(listOf(provider))
+                .setDefaultProvider(provider)
+                .setTheme(R.style.Theme_Safebay)
+                .build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        startActivityForResult(auth, RC_AUTH)
+    }
+
+    companion object {
+        private const val RC_AUTH = 42069
     }
 }
