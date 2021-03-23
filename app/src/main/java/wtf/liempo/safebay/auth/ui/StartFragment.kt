d608393ce2b0086ace6cf41d5ad89af16f92ffc7
Alec John <gonzalesalec@gmail.com>
2021-03-24 01:33:04 +0800

    Revert "Remove kotlin class for StartFragment"
    This reverts commit e7660e87

---
+++ app/src/main/java/wtf/liempo/safebay/auth/ui/StartFragment.kt
@@ -0,0 +1,20 @@
+package wtf.liempo.safebay.auth.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import wtf.liempo.safebay.R
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(
+        R.layout.fragment_auth_start,
+        container, false)
+
+}
