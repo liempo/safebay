a31d66ae6fd55ede8b30e273f75fa04e9659f5da
Alec John <gonzalesalec@gmail.com>
2021-03-24 01:40:18 +0800

    Fix variable naming

--- app/src/main/java/wtf/liempo/safebay/auth/ui/AuthLoginFragment.kt
+++ app/src/main/java/wtf/liempo/safebay/auth/ui/AuthLoginFragment.kt
@@ -29,7 +29,7 @@ class AuthLoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSignIn.setOnClickListener {
+        binding.buttonStandardSignIn.setOnClickListener {
             vm.startAuth(requireActivity(), Type.STANDARD)
         }
 
