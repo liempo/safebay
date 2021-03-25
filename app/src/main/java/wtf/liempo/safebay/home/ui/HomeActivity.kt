461b053d2cc800d48bc12a6552d4eae2d2e2db76
Alec John <gonzalesalec@gmail.com>
2021-03-25 15:31:16 +0800

    Setup the HomeActivity

---
+++ app/src/main/java/wtf/liempo/safebay/home/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package wtf.liempo.safebay.home.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wtf.liempo.safebay.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
