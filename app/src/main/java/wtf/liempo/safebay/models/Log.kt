e839df7301bb9f1219f392014982216d567b664c
Alec John <gonzalesalec@gmail.com>
2021-03-31 20:27:16 +0800

    Parcelize firebase data classes

--- app/src/main/java/wtf/liempo/safebay/models/Log.kt
+++ app/src/main/java/wtf/liempo/safebay/models/Log.kt
@@ -1,10 +1,13 @@
 package wtf.liempo.safebay.models
 
+import android.os.Parcelable
 import com.google.firebase.firestore.ServerTimestamp
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
+@Parcelize
 data class Log(
     val guestId: String? = null,
     val businessId: String? = null,
     @ServerTimestamp val date: Date? = null
-)
+) : Parcelable
--- app/src/main/java/wtf/liempo/safebay/models/Profile.kt
+++ app/src/main/java/wtf/liempo/safebay/models/Profile.kt
@@ -1,7 +1,11 @@
 package wtf.liempo.safebay.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Profile (
     val name: String? = null,
     val imageUri: String? = null,
     val address: Address? = null
-)
+) : Parcelable
